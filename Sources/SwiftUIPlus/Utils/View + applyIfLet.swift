c6fdfecd8e23a88bab958a60a11ee10bd7784935
Глеб Капустин <gleb.kapustin.98@mail.ru>
2025-04-07 11:26:42 +0300

    [MEM09-7577] - added applyIfLet

---
+++ Sources/SwiftUIPlus/Utils/View + applyIfLet.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+public extension View {
+    @ViewBuilder
+    func applyIfLet<T>(_ value: T?, transform: (Self, T) -> some View) -> some View {
+        if let value {
+            transform(self, value)
+        } else {
+            self
+        }
+    }
+}

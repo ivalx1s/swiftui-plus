7ee85119608cd18dab8235a0414d6fa3d19d626e
Artem Grishchenko <timmigri@mts.ru>
2025-09-16 17:44:58 +0300

    add horizontal model to scale appearance

--- Sources/SwiftUIPlus/ViewModifier/ScaleAppearanceModifier/ScaleAppearanceModifier.swift
+++ Sources/SwiftUIPlus/ViewModifier/ScaleAppearanceModifier/ScaleAppearanceModifier.swift
@@ -4,17 +4,24 @@ public extension View {
     func scaleAppearance(
         shown: Binding<Bool>,
         animation: Animation = .easeInOut,
-        anchor: Alignment = .center
+        anchor: Alignment = .center,
+        mode: ScaleAppearanceModifierMode = .vertical
     ) -> some View {
         self
             .modifier(
                 ScaleAppearanceModifier(
                     shown: shown,
                     animation: animation,
-                    anchor: anchor
+                    anchor: anchor,
+                    mode: mode
                 )
             )
     }
+}
+
+public enum ScaleAppearanceModifierMode {
+    case vertical
+    case horizontal
 }
 
 struct ScaleAppearanceModifier: ViewModifier {
@@ -24,11 +31,12 @@ struct ScaleAppearanceModifier: ViewModifier {
     @Binding var shown: Bool
     let animation: Animation
     let anchor: Alignment
+    let mode: ScaleAppearanceModifierMode
 
     func body(content: Content) -> some View {
         Color.clear
-            .frame(width: .none, height: rect?.height)
-            .frame(width: .none, height: frameSize?.height)
+            .frame(width: contentWidth, height: contentHeight)
+            .frame(width: frameWidth, height: frameHeight)
             .overlay(alignment: anchor) {
                 content
                     .storingSize(in: $rect)
@@ -50,6 +58,36 @@ struct ScaleAppearanceModifier: ViewModifier {
         switch shown {
             case true: .none
             case false: .zero
+        }
+    }
+}
+
+extension ScaleAppearanceModifier {
+    private var contentWidth: CGFloat? {
+        switch mode {
+            case .vertical: .none
+            case .horizontal: rect?.width
+        }
+    }
+
+    private var contentHeight: CGFloat? {
+        switch mode {
+            case .vertical: rect?.height
+            case .horizontal: .none
+        }
+    }
+
+    private var frameWidth: CGFloat? {
+        switch mode {
+            case .vertical: .none
+            case .horizontal: frameSize?.width
+        }
+    }
+
+    private var frameHeight: CGFloat? {
+        switch mode {
+            case .vertical: frameSize?.height
+            case .horizontal: .none
         }
     }
 }
